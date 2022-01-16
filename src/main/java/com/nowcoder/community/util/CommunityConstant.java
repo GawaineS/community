47fedfe3be7983ae2ead29c9524ad10e3dfe7f86
Gavin <gavinzlh@outlook.com>
2022-01-16 15:54:16 -0800

    完善注册功能

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+    /**
+     * Activation Succeed
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * Activation Repeat
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * Activation Fail
+     */
+    int ACTIVATION_FAILURE = 2;
+
+
+}
