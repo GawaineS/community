83e92669dde72bcd7f746c252c72cfe51c3c5502
Gavin <gavinzlh@outlook.com>
2022-01-27 17:49:06 -0800

    实现发帖功能

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -15,4 +15,6 @@ public interface DiscussPostMapper {
     // 如果只有一个参数，并且在if里面使用，则必须加别名
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    int insertDiscussPost (DiscussPost discussPost);
+
 }
