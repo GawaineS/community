44087623d0170d8502ce72c20d600fbd25385e75
Gavin <gavinzlh@outlook.com>
2022-01-29 02:57:30 -0800

    进行统一的错误处理，分离日志和服务端代码

---
+++ src/main/java/com/nowcoder/community/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,41 @@
+//处理服务器500的异常
+
+package com.nowcoder.community.controller.advice;
+import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler({Exception.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("服务器发生异常: " + e.getMessage());
+        for (StackTraceElement element : e.getStackTrace()) {
+            logger.error(element.toString());
+        }
+
+        // 获得请求的方式
+        // 如果是异步请求就返回JSON
+        // 如果是普通请求的话就返回html位置
+        String xRequestedWith = request.getHeader("x-requested-with");
+        if ("XMLHttpRequest".equals(xRequestedWith)) {
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(CommunityUtil.getJSONString(1, "服务器异常!")); // 生成JSON并返回
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error"); // 返回500 error html位置
+        }
+    }
+
+}
